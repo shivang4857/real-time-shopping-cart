86f597ff567b30eab40e67c2fe17ab7e9e67503f
shivang4857 <shivangd801@gmail.com>
2024-08-12 01:07:26 +0530

    working

--- index.js
+++ index.js
@@ -1,38 +1,72 @@
-import  {initializeApp} from  "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import  {getDatabase , ref , push} from  "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL:  "https://shopping-cart-3d97b-default-rtdb.asia-southeast1.firebasedatabase.app"
-}
+  databaseURL:
+    "https://shopping-cart-3d97b-default-rtdb.asia-southeast1.firebasedatabase.app",
+};
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 
 //creating the refernce point in the db where you are going to store the data.
-const shoppingListInDB = ref(database , "shoppingList");
+const shoppingListInDB = ref(database, "shoppingList");
 
 const inputElement = document.getElementById("input-feild");
 const addButtonEl = document.getElementById("add-button");
 const listElement = document.getElementById("list");
 
-addButtonEl.addEventListener("click", function(){
-    let inputvalue = inputElement.value;
-    appendshoppinglist(inputvalue);
-  
-    push(shoppingListInDB, {
-        name: inputvalue
-       
-    });
-    clearList();
-    console.log(inputvalue);
-})
+addButtonEl.addEventListener("click", function () {
+  let inputvalue = inputElement.value;
 
+  push(shoppingListInDB, inputvalue);
 
-function clearList(){
-    inputElement = "";
+  clearList();
+  console.log(inputvalue);
+});
+
+onValue(shoppingListInDB, function (snapshot) {
+  clearshoppingList();
+  if (snapshot.exists()) {
+    let iteamlist = Object.entries(snapshot.val());
+
+    for (let i = 0; i < iteamlist.length; i++) {
+      let currentIteam = iteamlist[i];
+
+      appendshoppinglist(currentIteam);
+    }
+  } else {
+    listElement.innerHTML = "No items in the list ...yet";
+  }
+});
+
+function clearshoppingList() {
+  listElement.innerHTML = "";
 }
 
-function appendshoppinglist(iteamvalue){
-    listElement.innerHTML += `<li>${inputvalue}</li>`;
-    
+function clearList() {
+  inputElement.value = "";
+}
+
+function appendshoppinglist(iteamvalue) {
+  //listElement.innerHTML += `<li>${iteamvalue}</li>`;
+  let itemId = iteamvalue[0];
+  let iteamValue = iteamvalue[1];
+
+  let newElement = document.createElement("li");
+
+  newElement.addEventListener("click", function () {
+    let exactlocationInDB = ref(database, `shoppingList/${itemId}`);
+
+    remove(exactlocationInDB);
+  });
+
+  newElement.textContent = iteamValue;
+  listElement.append(newElement);
 }
---
+++ readme.md
@@ -0,0 +1,19 @@
+firebase security rules 
+
+in test mode can onw ith the link can edit the database for 30 days 
+
+// fire base function which we have used in this project
+ref
+onvalue fetch the data from the db  onvalue( dbref , functions(snapshot) {
+    console.log(snapshot.value)
+})
+intializeapp -:
+get database
+
+you can change these limit by changing the rules in the rules tab of firebase console .
+
+to covert js object to string we can use object method indeed
+it has 3 method inside
+object.values
+object.keys
+object.entries 
