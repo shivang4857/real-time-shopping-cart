61e1a7795f928acdfa5c1616b4fa28afa36ba455
shivang4857 <shivangd801@gmail.com>
2024-08-12 04:16:59 +0530

    authentication

--- index.js
+++ index.js
@@ -1,57 +1,110 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import {
   getDatabase,
   ref,
   push,
   onValue,
   remove,
-} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup ,signOut} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 
 const appSettings = {
-  databaseURL:
-    "https://shopping-cart-3d97b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  apiKey: "AIzaSyC6eEREyWZg6M5xZRserZ6BbKwMVcyS67I",
+  authDomain: "shopping-cart-3d97b.firebaseapp.com",
+  databaseURL: "https://shopping-cart-3d97b-default-rtdb.asia-southeast1.firebasedatabase.app",
 };
 
 const app = initializeApp(appSettings);
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
 const database = getDatabase(app);
 
-//creating the refernce point in the db where you are going to store the data.
-const shoppingListInDB = ref(database, "shoppingList");
+
+
 
 const inputElement = document.getElementById("input-feild");
 const addButtonEl = document.getElementById("add-button");
 const listElement = document.getElementById("list");
 
+const viewloggedOut = document.getElementById("logged-out");
+const viewloggedIn = document.getElementById("logged-in");
+const googleloginbtn = document.getElementById("googlebtn");
+const logoutbtn = document.getElementById("log-out");
 
+// Trigger button click on pressing Enter
 inputElement.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      addButtonEl.click();
-    }
-  });
+  if (event.key === "Enter") {
+    addButtonEl.click();
+  }
+});
 
 addButtonEl.addEventListener("click", function () {
   let inputvalue = inputElement.value;
-
-  push(shoppingListInDB, inputvalue);
-
-  clearList();
-  console.log(inputvalue);
-});
-
-onValue(shoppingListInDB, function (snapshot) {
-  clearshoppingList();
-  if (snapshot.exists()) {
-    let iteamlist = Object.entries(snapshot.val());
-
-    for (let i = 0; i < iteamlist.length; i++) {
-      let currentIteam = iteamlist[i];
-
-      appendshoppinglist(currentIteam);
-    }
-  } else {
-    listElement.innerHTML = "No items in the list ...yet";
+  const userShoppingListinDB = ref(database,`users/${auth.currentUser.uid}/shoppingList`);
+  if (inputvalue.trim() !== "") {
+    push(userShoppingListinDB, inputvalue);
+    clearList();
+    console.log(inputvalue);
   }
 });
+
+// Google login
+googleloginbtn.addEventListener("click", function () {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log("Logged in with Google");
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+});
+
+// Monitor authentication state continuously
+onAuthStateChanged(auth, function (user) {
+  if (user) {
+ 
+    viewloggedIn.style.display = "block";
+    viewloggedOut.style.display = "none";
+    fetchFromDB();
+  } else {
+    viewloggedIn.style.display = "none";
+    viewloggedOut.style.display = "block";
+  }
+});
+
+// Logout out the user
+logoutbtn.addEventListener("click", function () {
+  signOut(auth)
+    .then(() => {
+      console.log("User signed out");
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+});
+
+// Monitor changes in the shopping list
+
+function fetchFromDB() {
+
+    clearshoppingList();
+    const userShoppingListinDB = ref(database,`users/${auth.currentUser.uid}/shoppingList`);
+    onValue(userShoppingListinDB, function (snapshot) {
+        clearshoppingList();
+        if (snapshot.exists()) {
+          let itemlist = Object.entries(snapshot.val());
+          for (let i = 0; i < itemlist.length; i++) {
+            let currentItem = itemlist[i];
+            appendshoppinglist(currentItem);
+          }
+        } else {
+          listElement.innerHTML = "No items in the list ...yet";
+        }
+      });
+      
+
+}
+
 
 function clearshoppingList() {
   listElement.innerHTML = "";
@@ -61,19 +114,18 @@ function clearList() {
   inputElement.value = "";
 }
 
-function appendshoppinglist(iteamvalue) {
-  //listElement.innerHTML += `<li>${iteamvalue}</li>`;
-  let itemId = iteamvalue[0];
-  let iteamValue = iteamvalue[1];
+function appendshoppinglist(itemvalue) {
+  let itemId = itemvalue[0];
+  let itemValue = itemvalue[1];
 
   let newElement = document.createElement("li");
 
   newElement.addEventListener("click", function () {
-    let exactlocationInDB = ref(database, `shoppingList/${itemId}`);
-
+    const exactlocationInDB = ref(database,`users/${auth.currentUser.uid}/shoppingList/${itemId}`);
+   
     remove(exactlocationInDB);
   });
 
-  newElement.textContent = iteamValue;
+  newElement.textContent = itemValue;
   listElement.append(newElement);
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,9 @@
 firebase security rules 
+ ".read" : "auth != null && auth.uid === $uid" ,
+ auth != null: This checks if the user is authenticated (i.e., auth is not null).
+auth.uid === $uid: This ensures that the authenticated user's UID matches the UID of the data they are trying to access. In other words, a user can only read their own data
+
+and same conditions goes for the write functions
 
 in test mode can onw ith the link can edit the database for 30 days 
 
@@ -17,3 +22,17 @@ it has 3 method inside
 object.values
 object.keys
 object.entries 
+
+
+// firebase  authentication
+
+first we will intialize the auth by import the required function 
+and put the api key in the intialiize db section then
+
+give the provider name which you are using
+and then write a function to call the signupwithpop
+we also want to call the onauthstatechange which moniter auth in the js file and change the state of our app
+
+// fetching user specific data from the db 
+nw we have to change our ref in the app and data with be pushed according the userid only.
+
